59bcc676b119899cfade7ae3eba4eb9d992b5bf1
Nurseit Kalbaev <kalban1991@gmail.com>
2021-03-20 14:35:07 -0700

    product list is done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import "./App.css";
+import Content from "./Content";
 import Header from "./Header";
 import Navbar from "./Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Cart from "./Cart";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Navbar />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Switch>
+          <Route path="/cart">
+            <Cart />
+          </Route>
+
+          <Route path="/">
+            <Navbar />
+            <Content />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -7,15 +7,19 @@ import {
   Person,
 } from "@material-ui/icons/";
 import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header grid">
-      <img
-        src="my-store-logo.png"
-        className="header__logo mt"
-        alt="my-store-logo"
-      />
+      <Link to="/">
+        <img
+          src="my-store-logo.png"
+          className="header__logo mt"
+          alt="my-store-logo"
+        />
+      </Link>
+
       <div className="header__search mt">
         <input className="header__input" type="text" />
         <Button className=".header__searchIconButton">
@@ -41,12 +45,14 @@ function Header() {
         </div>
 
         <div className="header__option">
-          <Button className="header__optionButton">
-            <span className="header__nav">
-              <ShoppingCart />
-            </span>
-            <span className=" header__cartCount">0</span>
-          </Button>
+          <Link to="/cart">
+            <Button className="header__optionButton">
+              <span className="header__nav">
+                <ShoppingCart />
+              </span>
+              <span className=" header__cartCount">0</span>
+            </Button>
+          </Link>
         </div>
       </div>
     </div>
